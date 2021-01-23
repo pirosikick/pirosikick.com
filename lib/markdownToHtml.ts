b0ff54559914e63834dfd813cf10a72984fc5db9
Hiroyuki Anai <pirosikick@gmail.com>
2021-01-23 10:51:53 +0900

    implement embeding twitter

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -6,11 +6,14 @@ import remark from "remark-parse";
 import html from "rehype-stringify";
 import shiki from "rehype-shiki";
 import remarkEmbedder, { Transformer } from "@remark-embedder/core";
+import fetch from "node-fetch";
+
+const wrapEmbededHTML = (name: string, innerHTML: string) =>
+  `<div data-embedded-media="${name}">${innerHTML}</div>`;
 
 const YouTubeTransformer: Transformer = {
   name: "YouTube",
   shouldTransform(url) {
-    console.log(url);
     const { host, pathname, searchParams } = new URL(url);
     return (
       host === "www.youtube.com" &&
@@ -32,20 +35,48 @@ const YouTubeTransformer: Transformer = {
       'allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"',
       "allowfullscreen",
     ].join(" ");
-    return `<div data-embedded-media="youtube"><iframe ${attrs}></iframe></div>`;
+    const iframe = `<iframe ${attrs}></iframe>`;
+
+    return wrapEmbededHTML("youtube", iframe);
+  },
+};
+
+const TwitterTransformer: Transformer = {
+  name: "Twitter",
+  shouldTransform(url) {
+    const { host, pathname } = new URL(url);
+    return host === "twitter.com" && /^\/[^/]+\/status\/\d+/.test(pathname);
+  },
+  async getHTML(url) {
+    const requestUrl = new URL("https://publish.twitter.com/oembed");
+    requestUrl.searchParams.append("url", url);
+    requestUrl.searchParams.append("align", "center");
+    requestUrl.searchParams.append("omit_script", "1");
+
+    const response = await fetch(requestUrl);
+    const json = await response.json();
+    assert(typeof json === "object" && json !== null);
+    assert(typeof json.html === "string");
+
+    return wrapEmbededHTML("twitter", json.html);
   },
 };
 
 export default async function markdownToHtml(
   markdown: string
-): Promise<string> {
+): Promise<{ html: string; twitter: boolean }> {
   const result = await unified()
     .use(remark)
-    .use(remarkEmbedder, { transformers: [YouTubeTransformer] })
+    .use(remarkEmbedder, {
+      transformers: [YouTubeTransformer, TwitterTransformer],
+    })
     .use(remark2rehype)
     .use(html)
     .use(shiki)
     .process(markdown);
 
-  return result.toString();
+  const resultHTML = result.toString();
+  const twitter = resultHTML.indexOf('data-embedded-media="twitter"') !== -1;
+
+  return { html: resultHTML, twitter };
 }
