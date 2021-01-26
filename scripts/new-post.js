6b26be1aafef8c9283b24d5c5ee1b7e990add9c1
Hiroyuki ANAI <pirosikick@gmail.com>
2021-01-27 01:04:49 +0900

    Journaling (#25)
    * change directory structure
    
    * update new-post.js
    
    * add how-to-do-journaling.md

--- scripts/new-post.js
+++ scripts/new-post.js
@@ -1,6 +1,7 @@
 /* eslint-env node */
 const fs = require("fs");
 const path = require("path");
+const makeDir = require("make-dir");
 const fileName = process.argv[2];
 
 if (!fileName) {
@@ -8,9 +9,10 @@ if (!fileName) {
   process.exit(1);
 }
 
+const today = new Date();
 const template = `---
 title: ""
-date: ${Math.floor(Date.now() / 1000)}
+date: ${Math.floor(today.getTime() / 1000)}
 excerpt: ""
 
 # coverImage: "/assets/${fileName}/some-image.jpg"
@@ -18,8 +20,9 @@ excerpt: ""
 ---
 `;
 
-fs.writeFileSync(
-  path.resolve(__dirname, `../_posts/${fileName}.md`),
-  template,
-  "utf8"
-);
+const y = today.getFullYear();
+const m = String(today.getMonth() + 1).padStart(2, "0");
+const dir = path.resolve(__dirname, `../_posts/${y}/${m}`);
+makeDir.sync(dir);
+
+fs.writeFileSync(path.join(dir, `${fileName}.md`), template, "utf8");
